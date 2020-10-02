4246bac2aae2005ab86b6b28efdb1475ae99ba31
bobo mallick <mallickbobo@gmail.com>
2020-10-02 14:35:55 +0530

    add

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEcApZ89n-mZLZetLhN2b3n0DwzUZKfXU",
+    authDomain: "burj-al-arab-auth1.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-auth1.firebaseio.com",
+    projectId: "burj-al-arab-auth1",
+    storageBucket: "burj-al-arab-auth1.appspot.com",
+    messagingSenderId: "890220279282",
+    appId: "1:890220279282:web:8fc8c3467bea100ba9a76b"
+  };
+
+  export default firebaseConfig;
